add6c1dd80e4fca05d5e072d3b59906f504d6231
Kristian Trenskow <trenskow@me.com>
2021-10-02 10:14:04 +0200

    Added the ability to manipulate keys in schemas.

--- test/index.js
+++ test/index.js
@@ -5,11 +5,11 @@ const chai = require('chai'),
 
 chai.use(chaiAsPromised);
 
-require('./key-paths');
 require('./ranges.js');
 require('./equals.js');
 require('./unique.js');
 require('./formalize.js');
 require('./validate.js');
 require('./middleware/');
+require('./key-paths');
 require('./merge');
--- test/key-paths.js
+++ test/key-paths.js
@@ -7,16 +7,16 @@ describe('keyPaths', () => {
 	it ('should come back with empty key paths', () => {
 		expect(keyPaths({
 			type: Boolean
-		})).to.eql(['']);
+		}).all()).to.eql(['']);
 	});
 	it ('should come back with three key paths.', () => {
 		expect(keyPaths({
 			'first': Boolean,
 			'second': String,
 			'third': Number
-		})).to.eql(['','first','second','third']);
+		}).all()).to.eql(['','first','second','third']);
 	});
-	it ('should come back with three eight paths.', () => {
+	it ('should come back with three key paths.', () => {
 		expect(keyPaths({
 			'first': [{
 				'first': Boolean,
@@ -27,9 +27,9 @@ describe('keyPaths', () => {
 				'second': String
 			},
 			'third': Number
-		})).to.eql(['','first','first.first','first.second','second','second.first','second.second','third']);
+		}).all()).to.eql(['','first','first.first','first.second','second','second.first','second.second','third']);
 	});
-	it ('should come back with three eight paths.', () => {
+	it ('should come back with three key paths.', () => {
 		expect(keyPaths({
 			'first': [{
 				'first': Boolean,
@@ -40,9 +40,9 @@ describe('keyPaths', () => {
 				'second': String
 			},
 			'third': Number
-		}, Object)).to.eql(['','first','second']);
+		}).all(Object)).to.eql(['','first','second']);
 	});
-	it ('should come back with three eight paths.', () => {
+	it ('should come back with two key paths.', () => {
 		expect(keyPaths({
 			'first': [{
 				'first': Boolean,
@@ -53,9 +53,9 @@ describe('keyPaths', () => {
 				'second': String
 			},
 			'third': Number
-		}, Boolean)).to.eql(['first.first','second.first']);
+		}).all(Boolean)).to.eql(['first.first','second.first']);
 	});
-	it ('should come back with three eight paths.', () => {
+	it ('should come back with three key paths.', () => {
 		expect(keyPaths({
 			'first': [{
 				'first': Boolean,
@@ -66,6 +66,30 @@ describe('keyPaths', () => {
 				'second': String
 			},
 			'third': Number
-		}, [Boolean, Array])).to.eql(['first','first.first','second.first']);
+		}).all([Boolean, Array])).to.eql(['first','first.first','second.first']);
+	});
+	it ('should come back with object at key (with intermediate array).', () => {
+		expect(keyPaths({
+			'first': [{
+				'first': Boolean,
+				'second': String
+			}]
+		}).get('first.second')).to.have.property('type').equal(String);
+	});
+	it ('should come back with object at key.', () => {
+		expect(keyPaths({
+			'second': {
+				'first': Boolean,
+				'second': String
+			}
+		}).get('second.second')).to.have.property('type').equal(String);
+	});
+	it ('should come back with root.', () => {
+		expect(keyPaths(String).get('')).to.have.property('type').equal(String);
+	});
+	it ('should come back with new validators set', () => {
+		expect(keyPaths({
+			'test': String
+		}).set('my', Number)).to.have.property('schema').to.have.property('my').to.have.property('type').to.equal(Number);
 	});
 });
